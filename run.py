0a80449159dcf36e4354538ed04992674da0d863
Muhammad Andre Widianto <andrewidianto12@gmail.com>
2024-03-13 21:38:21 +0700

    list by type

--- run.py
+++ run.py
@@ -16,15 +16,11 @@ app = init_app("app", config)
 
 # Define route to render index.html
 
+# POKEMON_API_URL = "https://pokeapi.co/api/v2/"
 
-def search_pokemon(query, category):
-    if category == 'pokemon':
-        url = f"https://pokeapi.co/api/v2/pokemon/{query.lower()}"
-    elif category == 'type':
-        url = f"https://pokeapi.co/api/v2/type/{query.lower()}"
-    else:
-        return None
 
+def get_pokemon_info(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -33,37 +29,64 @@ def search_pokemon(query, category):
         return None
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def get_pokemon_type(type_name):
+    url = f"https://pokeapi.co/api/v2/type/{type_name.lower()}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
 
 
-@app.route('/list_types', methods=['GET'])
-def list_types():
+def get_pokemon_types():
     url = "https://pokeapi.co/api/v2/type"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        types = [t['name'] for t in data['results']]
-        return render_template('types.html', types=types)
+        types = [type_data['name'] for type_data in data['results']]
+        return types
     else:
-        return render_template('error.html')
+        return []
+
+
+def search_pokemon(query, category):
+    if category == 'pokemon':
+        return get_pokemon_info(query)
+    elif category == 'type':
+        return get_pokemon_type(query)
+    else:
+        return None
+
+
+@app.route('/')
+def index():
+    types = get_pokemon_types()
+    return render_template('index.html', pokemon_types=types)
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
-        query = request.form['pokemon_name']
-        category = request.form['category']
-        result = search_pokemon(query, category)
-        if result:
-            if category == 'pokemon':
-                return render_template('result.html', pokemon=result)
-            elif category == 'type':
+        if 'pokemon_name' in request.form:  # Handling search by Pokemon name
+            query = request.form['pokemon_name']
+            category = request.form['category']
+            result = search_pokemon(query, category)
+            if result:
+                if category == 'pokemon':
+                    return render_template('result.html', pokemon=result)
+                elif category == 'type':
+                    return render_template('type_result.html', type_info=result)
+            else:
+                return render_template('error.html')
+        elif 'type' in request.form:  # Handling search by Pokemon type
+            type_name = request.form['type']
+            result = get_pokemon_type(type_name)
+            if result:
                 return render_template('type_result.html', type_info=result)
-        else:
-            return render_template('error.html')
-    return render_template('index.html')
+            else:
+                return render_template('error.html')
+    return render_template('index.html', pokemon_types=get_pokemon_types())
 
 
 if __name__ == "__main__":
